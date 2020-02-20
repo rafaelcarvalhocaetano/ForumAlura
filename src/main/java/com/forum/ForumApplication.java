a14db9ddf0a0c30ae760356e16f78ffd492139ae
rafaelcarvalhocaetano <rapha.pse@outlook.com>
2020-02-19 21:25:14 -0300

    Add models

--- src/main/java/com/forum/ForumApplication.java
+++ src/main/java/com/forum/ForumApplication.java
@@ -1,13 +1,13 @@
-package com.forum.forum;
+package com.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ForumApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ForumApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/forum/model/StatusTopico.java
@@ -0,0 +1,10 @@
+package com.forum.model;
+
+public enum StatusTopico {
+	
+	NAO_RESPONDIDO,
+	NAO_SOLUCIONADO,
+	SOLUCIONADO,
+	FECHADO;
+
+}

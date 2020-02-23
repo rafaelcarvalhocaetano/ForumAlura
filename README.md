af8d4ddf0be9a3e0ebb7bde678924c39807790d8
rafaelcarvalhocaetano <rapha.pse@outlook.com>
2020-02-23 01:45:38 -0300

    Add ADMIN-MONITOR
    Signed-off-by: rafael <rapha.pse@outlook.com>

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ForumAlura
 SpringBoot:leaves:Desenvolvimento de uma API 32630520200218149513, com persistência em banco de dados relacional (MySQL) usando.
+
+### PROJETO ADMIN_MONITOR
+ - O projeto monitora a API fornecendo diversos recursos para acompanhamento de equipe.
+ - 
--- src/main/java/com/forum/config/security/Security.java
+++ src/main/java/com/forum/config/security/Security.java
@@ -48,6 +48,7 @@ public class Security extends WebSecurityConfigurerAdapter {
     http.authorizeRequests()
         .antMatchers(HttpMethod.GET, "/topicos").permitAll()
         .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+        .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
         .antMatchers(HttpMethod.POST, "/auth").permitAll()
         .anyRequest().authenticated()
         .and().csrf().disable()
